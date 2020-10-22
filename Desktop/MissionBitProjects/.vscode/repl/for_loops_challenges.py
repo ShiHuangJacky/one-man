dc02608b52a9724d3757e4a49b25c2e1320cab3f
Mission Bit <missionbit@40a-MissionBit.attlocal.net>
2020-10-21 18:32:38 -0700

    hihi

---
+++ Desktop/MissionBitProjects/.vscode/repl/for_loops_challenges.py
@@ -0,0 +1,28 @@
+# def divisible_by_ten(nums):
+#     count = 0
+#     for num in nums:
+#         if num % 10 == 0:
+#             count += 1
+#     return count
+# print(divisible_by_ten([20, 25, 30, 35, 40]))
+
+# def add_greetings(names):
+#     yes = []
+#     for pie in names:
+#         yes.append("Hello, " + pie)
+#     return yes
+# print(add_greetings(["Owen", "Max", "Sophie"]))
+
+# def delete_starting_evens(lst):
+#     while len(lst) > 0 and lst[0] % 2 == 0:
+#         num = lst.pop(0)
+#     return lst
+# print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+
+# print(delete_starting_evens([4, 8, 10]))
+
+names_and_heights = [["jenny", 67], ["alexus", 70]]
+
+for student in names_and_heights:
+    for attribute in student:
+        print(attribute)
--- Desktop/MissionBitProjects/loops.py
+++ Desktop/MissionBitProjects/loops.py
@@ -5,13 +5,34 @@
 # rng = list(range(6))
 # print(rng)
 
-lst = [1, 2, 3, 4, 5, 6, 7, 8]
-for num in lst:
-    print(num*2)
-print(lst)
+# lst = [1, 2, 3, 4, 5, 6, 7, 8]
+# for num in lst:
+#     print(num*2)
+# print(lst)
 
-for p in range(len(lst)):
-    lst[p] = 2 * lst[p]
-    print(lst[p])
+# for p in range(len(lst)):
+#     lst[p] = 2 * lst[p]
+#     print(lst[p])
 
-print(lst)
+# print(lst)
+
+# my_favorite_numbers = [4, 8, 15, 42]
+
+# for num in my_favorite_numbers:
+#     my_favorite_numbers.append(1)
+
+# items_on_sale = ["blue_shirt", "striped_socks", "knit_dress", "red_headband"]
+
+# for item in items_on_sale:
+#     if item == "knit_dress":
+#         print("knit dress is on sale")
+#         break
+# print("end of search")
+
+# nums = [1, 2, 3, 4, 5, 6, 7]
+# for num in nums:
+#     if num%2 == 0:
+#         continue
+#     print(num)
+
+
